5896294b6a7a78a68710d67c2c24aa3b37f6ccd4
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-12-10 12:28:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ let good = document.getElementById("good");
 let cheap = document.getElementById("cheap");
 let fast = document.getElementById("fast");
 
+let g1 = document.getElementById("g1");
+let g2 = document.getElementById("g2");
+let g3 = document.getElementById("g3");
+
 let selected = [];
 
-function handleCheckbox(box) {
+function toggleCheckbox(box) {
+  box.checked = !box.checked;
+
   if (box.checked) {
     if (selected.length < 2) {
       selected.push(box);
     } else {
-      
       selected[0].checked = false;
-      selected.shift();     
+      selected.shift();
       selected.push(box);
     }
   } else {
-    
     selected = selected.filter(item => item !== box);
   }
 }
 
-good.addEventListener("change", () => handleCheckbox(good));
-cheap.addEventListener("change", () => handleCheckbox(cheap));
-fast.addEventListener("change", () => handleCheckbox(fast));
+
+g1.addEventListener("click", () => toggleCheckbox(good));
+g2.addEventListener("click", () => toggleCheckbox(cheap));
+g3.addEventListener("click", () => toggleCheckbox(fast));
