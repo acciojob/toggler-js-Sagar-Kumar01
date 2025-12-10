8323e1a089ef25596964ce5fff8f2557e9cc17cf
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-12-10 12:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+let good = document.getElementById("good");
+let cheap = document.getElementById("cheap");
+let fast = document.getElementById("fast");
+
+let selected = [];
+
+function handleCheckbox(box) {
+  if (box.checked) {
+    if (selected.length < 2) {
+      selected.push(box);
+    } else {
+      
+      selected[0].checked = false;
+      selected.shift();     
+      selected.push(box);
+    }
+  } else {
+    
+    selected = selected.filter(item => item !== box);
+  }
+}
+
+good.addEventListener("change", () => handleCheckbox(good));
+cheap.addEventListener("change", () => handleCheckbox(cheap));
+fast.addEventListener("change", () => handleCheckbox(fast));
